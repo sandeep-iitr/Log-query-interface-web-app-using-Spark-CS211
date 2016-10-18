03191d6ace2059bda8d1ef0343ce95180b43e0e0
Sandeep Singh Sandha <sandha.iitr@gmail.com>
2016-10-17 18:33:25 -0700

    Sandeep Starting code

---
+++ src/Answer_query_servlet.java
@@ -0,0 +1,73 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Answer_query_servlet
+ */
+@WebServlet("/Answer_query_servlet")
+public class Answer_query_servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Answer_query_servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		// get parfameters from request : we may use to extract string written by user.
+		//code String bookId = request.getParameter("bookId");
+		
+		//should check the string extracted here
+		/*
+		  if (bookId == null) { // a bookId parameter was not defined for the request
+			
+			}
+
+			try {
+			 // call the needed functions
+			
+			...
+			}
+			catch (Exception e) { // handle any exceptions returned
+			...
+			}
+		*/
+		
+		// Dynamically create HTML response to send back to browser
+		response.setContentType("text/html");
+		response.setBufferSize(8192);
+		PrintWriter out = response.getWriter();
+		out.println( "<html>" + "<head><title>" + "QueryResults" +"</title></head>");
+		
+		out.println( "<html>" + "<body>" + "Hi you query is running on Quantum computer.<br> Please be patient." +"</body>");
+		
+		
+	}
+
+}
---
+++ src/Database_interactor.java
@@ -0,0 +1,15 @@
+
+/*
+ * Class provide to do database queries
+ * */
+
+public class Database_interactor {
+
+	//To use Mysql we need below details:
+	/*
+	 * a) JDBC driver  (This is a jar file)
+	 * b) Connect function (need MySql db details: name, host, username, password etc)
+	 * b) Execute Query functions.
+	 * c) Disconnect functions
+	 * */
+}
